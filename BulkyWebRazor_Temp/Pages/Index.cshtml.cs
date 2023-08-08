97cd5449164c516bc40dc959699f11a41ee2341a
thorskii92 <nestorigna92@gmail.com>
2023-08-08 18:27:45 +0800

    try

--- BulkyWebRazor_Temp/Pages/Index.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Index.cshtml.cs
@@ -6,7 +6,6 @@ namespace BulkyWebRazor_Temp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
