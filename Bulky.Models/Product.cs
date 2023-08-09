0461ed9d90fc27007c2c1ec88fe97ca2208a6222
thorskii92 <nestorigna92@gmail.com>
2023-08-10 01:13:49 +0800

    added FK for category and imageUrl

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,5 +40,9 @@ namespace BulkyBook.Models
         [Range(1, 1000)]
         public double Price100 { get; set; }
 
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
